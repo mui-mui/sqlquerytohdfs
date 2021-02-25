ca3362899faa1cd5a401bb3cfc677ad60261a9c0
Alex <alex.ti@tuta.io>
2021-02-25 11:08:48 +0300

    Add metadata writer

--- Writer.py
+++ Writer.py
@@ -1,15 +1,15 @@
+from MetadataAdjusterHelper.MetadataAdjusterHelper import MetadataAdjusterHelper
 from ProcessHandler import AbstractProcessHandler
 from ProcessMessage import ProcessMessage
 from Options import WriteOption
 import requests
 import io
-from pyarrow import parquet
 
 
 class Writer(AbstractProcessHandler):
     """
     Класс писателя данных в hdfs
-    TODO:Добавить запись метаинформации в файл
+    Формирует файл parquet с метаданными и записывает в hdfs
     """
     def __init__(self, option: WriteOption):
         self.options = option
@@ -19,9 +19,14 @@ class Writer(AbstractProcessHandler):
             f"{self.options.serverHdfs}"
             f"{self.options.dirHdfs}/"
             f"{self.options.filename}.parquet?user.name={self.options.userHdfs}&op=CREATE&permission=0777", allow_redirects=False)
-        f = io.BytesIO()
-        request.Body.to_parquet(f, compression='gzip')
-        f.seek(0)
-        requests.put(resp.headers["Location"], data=f.read())
+        with io.BytesIO() as parquetFileIO:
+            request.Body.to_parquet(parquetFileIO, compression='gzip')
+            parquetFileIO.seek(0)
+            newParquetFileIO = MetadataAdjusterHelper.AddMetadataParquet(
+                parquetFileIO=parquetFileIO,
+                customMetadata={"PLATE_ID": request.Body["PLATE_ID"].values}
+            )
+        with newParquetFileIO as f:
+            requests.put(resp.headers["Location"], data=f.read())
 
         return super().Handle(request)
